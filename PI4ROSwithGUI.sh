0788296a130a534125df9f890ec17d9c481143d3
WeiDit <41475581+WeiDit@users.noreply.github.com>
2021-03-09 12:48:00 +0000

    Update PI4ROSwithGUI.sh

--- PI4ROSwithGUI.sh
+++ PI4ROSwithGUI.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 echo "Setup ROS Repositories"
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
